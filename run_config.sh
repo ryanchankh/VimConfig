0279dd8afd25dd6e927e5ea3ed1e59a97b438404
ryanchankh <ryanchankh@berkeley.edu>
2018-11-02 15:39:14 -0700

    update copy file directory

--- run_config.sh
+++ run_config.sh
@@ -1,17 +1,22 @@
+## SSH SERVER CONFIGURATION
+## AUTHOR: Ryan Chan
+## Last Update: 2 Novmeber 2018
+
 # Vim 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 rm ~/.vimrc
-cp ~/config/.vimrc ~/.vimrc
+cp ~/config/.vimrc ~
 vim +PlugInstall
-
 
 # Tmux 
 rm ~/.tmux.conf
-cp ./tmux.conf ~/.tmux.conf
+cp ./tmux.conf ~
 
 # Aliases
 rm ~/.aliases
-cp ./.aliases ~/.aliases
+cp ./.aliases ~
 source ~/.aliases
+
+cd ~
